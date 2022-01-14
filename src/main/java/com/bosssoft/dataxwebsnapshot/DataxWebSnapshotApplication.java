f9aab116c27c5640f2998e87d216bb7541b241ae
tt <fjfztqh@126.com>
2022-01-14 09:25:14 +0800

    2022-01-14 init

---
+++ src/main/java/com/bosssoft/dataxwebsnapshot/DataxWebSnapshotApplication.java
@@ -0,0 +1,13 @@
+package com.bosssoft.dataxwebsnapshot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataxWebSnapshotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataxWebSnapshotApplication.class, args);
+    }
+
+}
